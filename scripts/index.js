ccc39e4474f012ec57ea872bcbef352be1cb3317
Ksenya Gorkova <doroni4eva2016@yandex.ru>
2022-03-30 14:56:45 +0300

    second-otprav

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,26 @@
 const popupElement = document.querySelector('.popup');
 const button = document.querySelector('.profile__edit-button');
 const closeButton = popupElement.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup-box__theme_name');
+let jobInput = document.querySelector('.popup-box__theme_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 function openPopup() {
-    popupElement.classList.add('popup__opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    popupElement.classList.add('popup_opened');
 }
 function closePopup() {
-    popupElement.classList.remove('popup__opened');
+    popupElement.classList.remove('popup_opened');
 }
-button.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-let formElement = document.querySelector(".popup__container");
-let nameInput = document.querySelector(".popup__box_name");
-let jobInput = document.querySelector(".popup__box_job");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    close();
+    closePopup();
 }
-
+button.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener("submit", formSubmitHandler); 
