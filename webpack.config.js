bcaf1cc0b9c45510639ddd56e774a7d06416a64a
Ksenya Gorkova <doroni4eva2016@yandex.ru>
2022-09-04 23:15:03 +0300

    pr-8

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,19 @@ module.exports = {
         exclude: '/node_modules/',
         },
         {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|gif)$/,
         type: 'asset/resource',
+        generator: {
+            filename: 'images/[name].[hash][ext]'
         },
+        },
+        {
+            test: /\.(woff2|woff|eot|ttf|otf)$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'fonts/[name].[hash][ext]'
+            },
+            },
         {
         test: /\.css$/,
         use: [
